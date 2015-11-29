1e855d67c18f79828883a6fed4a9024e2dd7285a
Andrew Brookins <a.m.brookins@gmail.com>
2015-11-28 23:15:07 -0800

    WIP on a custom width override setting

---
+++ src/com/andrewbrookins/idea/wrap/ui/WrapSettingsPanel.java
@@ -0,0 +1,50 @@
+package com.andrewbrookins.idea.wrap.ui;
+
+import com.intellij.openapi.util.Comparing;
+
+import javax.swing.*;
+
+import com.andrewbrookins.idea.wrap.config.WrapSettingsProvider;
+
+
+/**
+ * Plugin settings UI.
+ */
+public class WrapSettingsPanel {
+
+    private WrapSettingsProvider settingsProvider;
+    private JTextField columnWidthOverrideField;
+    private JTextPane textPane;
+    private JPanel panel;
+
+    public WrapSettingsPanel() {
+        settingsProvider = WrapSettingsProvider.getInstance();
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public boolean isModified() {
+        return !Comparing.equal(columnWidthOverrideField.getText(), settingsProvider.getState().columnWidthOverride);
+    }
+
+    public void apply() {
+        // TODO: Show an error for non-integer input.
+        Integer columnWidth;
+
+        try {
+            columnWidth = Integer.parseInt(columnWidthOverrideField.getText());
+        }
+        catch (NumberFormatException e) {
+            columnWidth = null;
+        }
+
+        settingsProvider.getState().columnWidthOverride = columnWidth;
+    }
+
+    public void reset() {
+        columnWidthOverrideField.setText(String.valueOf(settingsProvider.getState().columnWidthOverride));
+    }
+}
+
